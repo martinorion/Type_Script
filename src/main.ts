c92290b4357dd3ce23e6f9ea289bf2df7f37ea93
MartinHolena <martinkiko18@gmail.com>
2021-09-28 20:47:30 +0200

    branch isTriangle

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
-function Numbers(a: number, b: number, c:number){
-    if(a == b && a != c || b == c && c != a || a == c && c != b){
-            console.log(2);
-        }
-         else if(a != b && a != c && b != c){
-            console.log(0);
-        }
-        else if (a == b && a == c){
-            console.log(3);
-        }
-}
-
-Numbers(1,2,1);
+function Triangle(a:number, b:number,c:number):boolean {
+    if(a + b > c && b + c > a && a + c > b) {
+               return true;
+   }
+   return false;
+   }
+   
+   console.log(Triangle(2,5,4))
